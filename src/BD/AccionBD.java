3ea677e5f0ec41d8e05b9d8ef1b0cbeb5a5dd32f
Atik T <atiktuquerres@gmail.com>
2024-11-24 11:36:51 -0500

    Add: Tabla de acciones y Registro de acciones

--- src/BD/AccionBD.java
+++ src/BD/AccionBD.java
@@ -1,5 +1,98 @@
 package BD;
 
+import Modelo.Accion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 public class AccionBD {
-    
+
+    public boolean registrarCompra(Accion accion) {
+        PreparedStatement ps = null;
+        Connection con = Conexion.getConexion();
+        String sql = "INSERT INTO compras (id_usuario, nombre_accion, fecha_compra, cantidad, valor) "
+                + "VALUES (?, ?, ?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, accion.getId_usuario());
+            ps.setString(2, accion.getNombre_accion());
+            ps.setString(3, accion.getFecha_compra());
+            ps.setInt(4, accion.getCantidad());
+            ps.setDouble(5, accion.getValor());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar la compra de la acción");
+            return false;
+        }
+    }
+
+    public void mostrarCompras(JTable tabla) {
+        Connection con = Conexion.getConexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT id, id_usuario, nombre_accion, fecha_compra, cantidad, valor FROM compras";
+
+        DefaultTableModel modelo = new DefaultTableModel();
+        modelo.addColumn("ID");
+        modelo.addColumn("ID Usuario");
+        modelo.addColumn("Nombre Acción");
+        modelo.addColumn("Fecha Compra");
+        modelo.addColumn("Cantidad");
+        modelo.addColumn("Valor");
+
+        tabla.setModel(modelo);
+
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Object[] fila = new Object[6];
+                fila[0] = rs.getInt("id");
+                fila[1] = rs.getInt("id_usuario");
+                fila[2] = rs.getString("nombre_accion");
+                fila[3] = rs.getDate("fecha_compra").toString();
+                fila[4] = rs.getInt("cantidad");
+                fila[5] = rs.getDouble("valor");
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al mostrar los datos de compras");
+        }
+    }
+
+    public Accion obtenerCompraPorId(int idCompra) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = Conexion.getConexion();
+        String sql = "SELECT id, id_usuario, nombre_accion, fecha_compra, cantidad, valor FROM compras WHERE id = ?";
+        Accion accion = null;
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idCompra); // Establece el ID de la compra
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                accion = new Accion();
+                accion.setId(rs.getInt("id"));
+                accion.setId_usuario(rs.getInt("id_usuario"));
+                accion.setNombre_accion(rs.getString("nombre_accion"));
+                accion.setFecha_compra(rs.getString("fecha_compra"));
+                accion.setCantidad(rs.getInt("cantidad"));
+                accion.setValor(rs.getDouble("valor"));
+            } else {
+                JOptionPane.showMessageDialog(null, "Acción no encontrada", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al obtener los datos de la compra", "Error", JOptionPane.ERROR_MESSAGE);
+        } 
+        return accion;
+    }
 }
---
+++ src/Modelo/Accion.java
@@ -0,0 +1,111 @@
+package Modelo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Accion {
+
+    private int id;
+    private int id_usuario;
+    private String nombre_accion;
+    private String fecha_compra;
+    private int cantidad;
+    private double valor;
+
+    public Accion() {
+    }
+
+    public Accion(int id_usuario, String nombre_accion, String fecha_compra, int cantidad, double valor) {
+        this.id_usuario = id_usuario;
+        this.nombre_accion = nombre_accion;
+        this.fecha_compra = fecha_compra;
+        this.cantidad = cantidad;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre_accion() {
+        return nombre_accion;
+    }
+
+    public void setNombre_accion(String nombre_accion) {
+        this.nombre_accion = nombre_accion;
+    }
+
+    public String getFecha_compra() {
+        return fecha_compra;
+    }
+
+    public void setFecha_compra(String fecha_compra) {
+        this.fecha_compra = fecha_compra;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean esFechaValida(String fecha) {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate fechaIngresada = LocalDate.parse(fecha, formato);
+            LocalDate fechaActual = LocalDate.now();
+
+            if (fechaIngresada.isAfter(fechaActual)) {
+                return false;
+            }
+            return true;
+
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public boolean esTextoValido(String texto) {
+        if (texto == null) {
+            return false;
+        }
+        String regex = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]{1,50}$";
+        return texto.matches(regex);
+    }
+
+    public boolean esValorValido(double numero) {
+        return numero > 0;
+    }
+
+    public boolean esCantidadValido(String texto) {
+        if (texto == null) {
+            return false;
+        }
+        String regex = "^[1-9][0-9]*$";
+        return texto.matches(regex);
+    }
+
+}
