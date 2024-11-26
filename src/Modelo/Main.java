a4687044c6247a8f5bc201d8582689490e039082
AngelCabezas <120534237+AngelCabezas@users.noreply.github.com>
2024-11-25 23:29:55 -0500

    Update Main.java

--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -5,22 +5,31 @@ import Controlador.AccionController;
 import Vista.JFAcciones;
 import Vista.JFDetalleAccion;
 import Vista.JFRegistrarAccion;
+import Vista.JFramelogin;
+import listener.LoginSuccessListener;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        JFAcciones jfAcciones = new JFAcciones();
-        AccionBD accionBD = new AccionBD();
-        JFRegistrarAccion jfRegistrarAccion = new JFRegistrarAccion();
-        JFDetalleAccion jfDetalleAccion = new JFDetalleAccion();
-        Accion accion = new Accion();
-        AccionController accionController = new AccionController(accionBD, jfAcciones,jfRegistrarAccion,jfDetalleAccion,accion);
-        jfAcciones.setVisible(true);
-        jfAcciones.setLocationRelativeTo(null);
-        jfDetalleAccion.setLocationRelativeTo(null);
-        jfRegistrarAccion.setLocationRelativeTo(null);
-    } 
+
+        LoginSuccessListener listener = new LoginSuccessListener() {
+            @Override
+            public void onLoginSuccess() {
+                JFAcciones jfAcciones = new JFAcciones();
+                AccionBD accionBD = new AccionBD();
+                JFRegistrarAccion jfRegistrarAccion = new JFRegistrarAccion();
+                JFDetalleAccion jfDetalleAccion = new JFDetalleAccion();
+                Accion accion = new Accion();
+                AccionController accionController = new AccionController(accionBD, jfAcciones, jfRegistrarAccion, jfDetalleAccion, accion);
+
+                jfAcciones.setVisible(true);
+                jfAcciones.setLocationRelativeTo(null);
+                jfRegistrarAccion.setLocationRelativeTo(null);
+                jfDetalleAccion.setLocationRelativeTo(null);
+            }
+        };
+
+        JFramelogin jfLogin = new JFramelogin(listener);
+        jfLogin.setVisible(true);
+    }
 }
