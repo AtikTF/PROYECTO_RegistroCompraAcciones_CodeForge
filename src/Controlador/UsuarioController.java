dbed0c5df5d6a06696895a1c0f55be9098a9a110
AngelCabezas <120534237+AngelCabezas@users.noreply.github.com>
2024-11-25 23:22:50 -0500

    Update UsuarioController.java

--- src/Controlador/UsuarioController.java
+++ src/Controlador/UsuarioController.java
@@ -1,5 +1,20 @@
 package Controlador;
 
+import BD.UsuarioBD;
+import Modelo.Usuario;
+
 public class UsuarioController {
-    
+
+    private UsuarioBD usuarioBD;
+
+    // Constructor
+    public UsuarioController(UsuarioBD usuarioBD) {
+        this.usuarioBD = usuarioBD;
+    }
+
+    // Método para autenticar al usuario
+    public boolean autenticar(String nombreUsuario, String contraseña) {
+        Usuario usuario = usuarioBD.obtenerUsuarioPorNombre(nombreUsuario);
+        return usuario != null && usuario.getContraseña().equals(contraseña);
+    }
 }
