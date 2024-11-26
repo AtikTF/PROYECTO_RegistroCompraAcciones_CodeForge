7eca3c0fdda26983b42f476d5721bb0e217a889c
AngelCabezas <120534237+AngelCabezas@users.noreply.github.com>
2024-11-25 23:25:15 -0500

    Update Usuario.java

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -2,4 +2,26 @@ package Modelo;
 
 public class Usuario {
     
+    private int id;
+    private String nombreUsuario;
+    private String contraseña;
+
+    // Constructor, getters y setters
+    public Usuario(int id, String nombreUsuario, String contraseña) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
 }
